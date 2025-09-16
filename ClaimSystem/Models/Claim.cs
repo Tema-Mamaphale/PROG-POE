676cfdb8a51acaf2208769f3c3b49b4aa6680e03
Tema-Mamaphale <165869983+Tema-Mamaphale@users.noreply.github.com>
2025-09-16 12:22:37 +0200

    Add models
    The models created

---
+++ ClaimSystem/Models/Claim.cs
@@ -0,0 +1,19 @@
+﻿namespace ClaimSystem.Models
+{
+    // an enum is a set of named constants, works instead of using plain strings or numbers. this will clearly represnt what stage a claim is in
+    public enum ClaimStatus { Draft, Submitted, PendingReview, Approved, Rejected }
+
+    public class Claim
+    {
+        public int Id { get; set; }
+        public string LecturerName { get; set; } = "";
+        public string Month { get; set; } = "";
+        public decimal HoursWorked { get; set; }
+        public decimal HourlyRate { get; set; }
+        public string? Notes { get; set; }
+        public string? AttachmentFileName { get; set; }
+        public ClaimStatus Status { get; set; } = ClaimStatus.Draft;
+
+        public decimal CalculatedAmount => HoursWorked * HourlyRate;
+    }
+}
